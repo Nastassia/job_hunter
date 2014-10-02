1f78177b6a4b460b21f5fc3de69be2629a1e49c3
Nastassia <nastassiacarmona@gmail.com>
2014-10-02 19:07:25 -0400

    Tried to get template to work, failed. Built out Category view and model, rendered Categories to the DOM.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -11,9 +11,18 @@ var ContactModel = Backbone.Model.extend({
   urlRoot: "/contacts"
 });
 
+var CategoryModel = Backbone.Model.extend({
+  urlRoot: "/categories"
+});
+
 var ContactCollection = Backbone.Collection.extend({
   url: "/contacts",
   model: ContactModel
+});
+
+var CategoryCollection = Backbone.Collection.extend({
+  url: "/categories",
+  model: CategoryModel
 });
 
 contact_collection = new ContactCollection();
@@ -25,12 +34,32 @@ contact_collection.fetch({success: function(){
   })
 }})
 
+category_collection = new CategoryCollection();
+
+category_collection.fetch({success: function(){
+  category_collection.models.forEach(function(item){
+    var catView = new CategoryView({model: item});
+    catView.render();
+  })
+}}
+)
+
 
 var ContactView = Backbone.View.extend({
   tagName: 'li',
   render: function(){
+    // var template = _.template($("#template").html(), {models: this.model})
+    // $("contacts").append(this.$el.html(template));
     var innards = "<h3>"+this.model.get('name')+" "+"--"+this.model.get('category_id')+"</h3>"
     // this.$el.html(innards).append($('#contacts'));
     $("#contacts").append(this.$el.html(innards));
+  }
+})
+
+var CategoryView = Backbone.View.extend({
+  tagName: 'div',
+  render: function(){
+    var innards = "<p>"+this.model.get('name')+"</p>"
+    $("#categories").append(this.$el.html(innards));
   }
 })
