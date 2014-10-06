c71fa477cab74e95fd26db6b18a8f67175e9637e
Nastassia <nastassiacarmona@gmail.com>
2014-10-06 16:08:33 -0400

    Played with styles and bootstrap, added Category as a dropdown

--- public/javascripts/scripts3.js
+++ public/javascripts/scripts3.js
@@ -8,7 +8,7 @@ $(function(){
     // render the contacts from server
     var contactsArea = $('#contacts');
     for(var i=0;i<data.length;i++){
-      contactsArea.append('<div class="divContactItem"><li class="contactItem" draggable="true">' + data[i].name +'<br />'+'</li>'+'<button id="del">X</button></div>');
+      contactsArea.append('<div class="divContactItem"><li class="contactItem" draggable="true">' + data[i].name +'<br />'+'</li>'+'<button id="edit">Edit</button>'+" "+'<button id="del"><img src="/trashcan.gif" /></button></div>');
 
     }
 
@@ -32,10 +32,13 @@ $(function(){
       });
   });
 }
+
+var dropdown = $('select');
   $.get('/categories', function(data){
     var categoryArea = $('#categories');
     for(var i=0;i<data.length;i++){
       categoryArea.append('<li class="categoryItem">' + data[i].name + '</li>');
+      dropdown.append('<option value="'+data[i].id+'">'+data[i].name+'</option>');
     }
   });
 
@@ -47,9 +50,17 @@ $(function(){
     var contactNumber = $('input[name="number"]').val();
     var contactPicture = $('input[name="picture"]').val();
     var contactCategory = $('input[name="category"]').val();
+
+    // var inputs = $('input');
+    // for(var i=0;i<inputs.length;i++){
+    //   if (inputs[i] == ""){
+    //     alert('')
+    //   }
+    // }
+    //
     var newContact = contactName
     $.post('/contacts',
-      {name: contactName, age: contactAge, address: contactAddress, phone_number: contactNumber, picture: contactPicture, category_id: contactCategory}, function(){ $('div#contacts').append('<div class="divContactItem"><li class="contactItem" draggable="true">' + contactName +'<br />'+'</li>'+'<button id="del">X</button></div>'); }
+      {name: contactName, age: contactAge, address: contactAddress, phone_number: contactNumber, picture: contactPicture, category_id: contactCategory}, function(){ $('div#contacts').append('<div class="divContactItem"><li class="contactItem" draggable="true">' + contactName +'<br />'+'</li>'+'<button id="edit">Edit</button>'+" "+'<button id="del"><img src="/trashcan.gif" /></button></div>'); }
     ); $('input').val('');
   });
 
--- seeds.rb
+++ seeds.rb
@@ -6,9 +6,11 @@ require_relative './lib/contact'
 Category.delete_all
 Contact.delete_all
 
-friends = Category.create(name: "friends")
-frenemies = Category.create(name: "frenemies")
+alumni = Category.create(name: "Alumni")
+networking = Category.create(name: "Networking")
+hr = Category.create(name: "HR")
 
-Contact.create(name: "Sean", category_id: friends.id)
-Contact.create(name: "Jeff", category_id: friends.id)
-Contact.create(name: "Neel", category_id: frenemies.id)
+
+Contact.create(name: "Sean", category_id: alumni.id)
+Contact.create(name: "Jeff", category_id: alumni.id)
+Contact.create(name: "Neel", category_id: alumni.id)
